29388cf92322f7fda8252aadde00885a6b3fa7c1
Russ <rronalds@gmail.com>
2020-03-01 20:34:23 -0500

    list of all realized languages

---
+++ SRs/all_real_para.py
@@ -0,0 +1,12 @@
+all =  open("COLAG_2011_flat.txt")
+p_list = []
+for line in all.readlines():
+    line = line.split("\t")
+    if line[0] not in p_list:
+        p_list.append(line[0])
+p_list.sort()
+with open("all_real_p.txt", 'w') as f:
+    for x in p_list:
+        f.write(x+"\n")
+print "\noutput file: all_real_p.txt with "+str(len(p_list))+" languages\n"
+
