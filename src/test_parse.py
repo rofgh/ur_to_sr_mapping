b170a742a90d62a819f5ab32dae55602e975f536
Russ <rronalds@gmail.com>
2020-04-19 22:04:46 -0400

    update test

--- src/test_parse.py
+++ src/test_parse.py
@@ -5,22 +5,23 @@ def test():
     outcome         = ""
     failed_line     = ""
     fail_count      = 0
-    count           = 0
+    total_count     = 0
+    total_fail_count= 0
     with open(testfilename, 'r') as t:
         with open(outputfilename, 'r') as o:
-            total_count = len(o.readlines())
+            total_count         = len(o.readlines())
             for test_line in t.readlines():
+                total_fail_count += 1
                 if test_line in o.readlines():
                     pass
                 else:
-                    count += 1
-    if count > 0:
-        outcome = "Failure:  {f} of the test lines are not present in the output!"
-    if count == 0:
+                    fail_count += 1
+    if fail_count > 0:
+        outcome = "Failure:  {f} out of {t} of the test lines are not present in the output!"
+    if fail_count == 0:
         outcome = "Success:  It seems all the test lines are present in the output!"
-    print(outcome)
 
-    print(outcome.format(f= fail_count))
+    print(outcome.format(f= fail_count, t=total_fail_count))
     '''
     txt = "For only {price:.2f} dollars!"
     print(txt.format(price = 49))
